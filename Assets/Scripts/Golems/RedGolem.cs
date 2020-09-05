d8d87bdf0a97519e86b9776707204c1b8fb8b863
Ryder James <stidwell@student.neumont.edu>
2020-09-05 14:54:40 -0600

    Add golems moving multiple cells per step

--- Assets/Scripts/Golems/RedGolem.cs
+++ Assets/Scripts/Golems/RedGolem.cs
@@ -2,21 +2,27 @@
 
 public class RedGolem : GolemBase {
 	public override void DoTurn(ref GridCell[,] neighbors) {
-		int[] directionOrder = new int[] { 0, 1, 2, 3 };
+		for (int step = 0; step < cellsPerMove; step++) {
+			if (step > 0) {
+				neighbors = Grid.GetNeighbors(entity);
+			}
 
-		for (int i = 0; i < directionOrder.Length; i++) {
-			int rand = Random.Range(0, directionOrder.Length);
-			int temp = directionOrder[i];
-			directionOrder[i] = directionOrder[rand];
-			directionOrder[rand] = temp;
-		}
+			int[] directionOrder = new int[] { 0, 1, 2, 3 };
 
-		for (int i = 0; i < 4; i++) {
-			Vector3 dir = Direction.All[directionOrder[i]];
-			bool canMove = CheckEmpty(ref neighbors, dir);
-			if (canMove) {
-				Grid.Move(entity, dir);
-				break;
+			for (int i = 0; i < directionOrder.Length; i++) {
+				int rand = Random.Range(0, directionOrder.Length);
+				int temp = directionOrder[i];
+				directionOrder[i] = directionOrder[rand];
+				directionOrder[rand] = temp;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				Vector3 dir = Direction.All[directionOrder[i]];
+				bool canMove = CheckEmpty(ref neighbors, dir);
+				if (canMove) {
+					Grid.Move(entity, dir);
+					break;
+				}
 			}
 		}
 	}

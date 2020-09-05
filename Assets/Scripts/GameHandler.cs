979a32be9a91a332a8d8d2553313522b4c2a9ede
Ryder James <stidwell@student.neumont.edu>
2020-09-05 16:46:49 -0600

    Add golem selecting

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour {
 	[SerializeField] private EntityGrid grid = null;
+	[SerializeField] private GameObject[] golemLenses = null;
 	[SerializeField] private GameObject[] golemPrefabs = null;
+
+	private int SelectedGolem { get; set; } = 0;
 
 	public void CreateGolem(Vector2 pos) {
 		if (grid.MovingEntities > 0) {
 			return;
 		}
 
-		GameObject golem = Instantiate(golemPrefabs[Random.Range(0, golemPrefabs.Length)]);
+		GameObject golem = Instantiate(golemPrefabs[SelectedGolem]);
 
 		bool added = grid.AddEntity(golem.GetComponent<GridEntity>(), pos);
 
@@ -18,6 +21,13 @@ public class GameHandler : MonoBehaviour {
 			Destroy(golem);
 		} else {
 			Step();
+		}
+	}
+
+	public void SelectGolem(int golemIndex) {
+		SelectedGolem = golemIndex;
+		for (int i = 0; i < golemLenses.Length; i++) {
+			golemLenses[i].SetActive(i != SelectedGolem);
 		}
 	}
 

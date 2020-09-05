d10f086896c226db2d9c424adba05fe7c92d25a5
Ryder James <stidwell@student.neumont.edu>
2020-09-05 16:28:31 -0600

    Disallow adding golems during a step

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameHandler : MonoBehaviour {
 	[SerializeField] private EntityGrid grid = null;
 	[SerializeField] private GameObject[] golemPrefabs = null;
 
 	public void CreateGolem(Vector2 pos) {
+		if (grid.MovingEntities > 0) {
+			return;
+		}
+
 		GameObject golem = Instantiate(golemPrefabs[Random.Range(0, golemPrefabs.Length)]);
 
 		bool added = grid.AddEntity(golem.GetComponent<GridEntity>(), pos);
